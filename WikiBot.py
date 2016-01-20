49e09cc24d663dc06493f6ce7538a76fa85e96f9
Alexander Decker <alexanderdecker@Alexanders-MacBook-Pro-6.local>
2016-01-20 12:43:48 -0600

    added error handling and started adding weather reporting

--- WikiBot.py
+++ WikiBot.py
@@ -10,26 +10,42 @@ print sc.api_call("api.test")
 print sc.api_call("auth.test")
 
 print sc.api_call("chat.postMessage",
-            username='wikiBot',
-            as_user='false',
-            icon_emoji=':bowtie:',
-            channel="C0JT331L0",
-            text="Hello World!")
+                  username='wikiBot',
+                  as_user='false',
+                  icon_emoji=':bowtie:',
+                  channel="C0JT331L0",
+                  text="Hello World!")
 
 if sc.rtm_connect():
     while True:
         time.sleep(1);
         new_evts = sc.rtm_read()
         for evt in new_evts:
-          print(evt)
-          if "type" in evt:
-            if evt["type"] == "message" and "text" in evt:
-              message=evt["text"]
-              if "wikiBot" in message[ :8]:
-                  print message[7:]
-                  print sc.api_call("chat.postMessage",
-                              username='WikiBot',
-                              as_user='false',
-                              icon_emoji=':bowtie:',
-                              channel=evt["channel"],
-                              text=wikipedia.summary(message[7:]).encode('utf-8'))
+            print(evt)
+            if "type" in evt:
+                if evt["type"] == "message" and "text" in evt:
+                    message=evt["text"]
+                    if "wikiBot" in message[ :8]:
+                        try:
+                            sc.api_call("chat.postMessage",
+                                        username='WikiBot',
+                                        as_user='false',
+                                        icon_emoji=':bowtie:',
+                                        channel=evt["channel"],
+                                        text=wikipedia.summary(message[7:]).encode('utf-8'))
+                        except:
+                            sc.api_call("chat.postMessage",
+                                        username='WikiBot',
+                                        as_user='false',
+                                        icon_emoji=':cloud:',
+                                        channel=evt["channel"],
+                                        text="Hmm, I couldn't find that, try another term...")
+                    '''
+                    if "weatherBot" in message [:11]:
+                        sc.api_call("chat.postMessage",
+                                    username='WeatherBot',
+                                    as_user='false',
+                                    icon_emoji=':bowtie:',
+                                    channel=evt["channel"],
+                                    text="It is currently: ...")
+                    '''
